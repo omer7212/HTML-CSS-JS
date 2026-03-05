e69a7399a04d1b9a0ca885d0e822ca3185c4b637
omer7212 <omershala77@gmail.com>
2026-03-05 17:41:09 +0100

    module26

--- module26/main.js
+++ module26/main.js
@@ -1,1 +1,104 @@
-console.log("Hello")
+console.log("Hello");
+
+
+var x = 10;
+
+var y= 30;
+
+var value = 30;
+
+
+console.log(x+y)
+
+console.log(x-y)
+
+console.log(x*y)
+
+console.log(x/y)
+
+
+
+var a = 99;
+
+a++;
+
+console.log(a)
+
+
+var b = 201
+
+b--;
+
+
+console.log(b)
+
+
+var c = 500;
+
+c+=5;
+
+console.log(c)
+
+
+
+
+var d = 50;
+
+d-=5;
+
+
+console.log(d)
+
+
+
+var e = 50;
+
+e*=5;
+
+
+console.log(e)
+
+
+
+
+var f = 70
+
+
+f/=5;
+
+console.log(f)
+
+
+
+var v = 100;
+
+
+var w = 50;
+
+// operatort e krahasimit
+
+console.log(   v==w   )
+
+console.log(   v!=w   )
+
+console.log(   v>w   )
+
+console.log(   v<w   )
+
+
+
+var emri = "Rigon" ;
+
+var mbiemri = "Hodza" ;
+
+
+var fullName = emri +" "+ mbiemri;
+
+console.log(fullName);
+
+
+console.log(`Hello from ${emri}`)
+
+
+
+
