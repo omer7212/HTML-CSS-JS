473ffb39e9be617fd2a987c8b0d8ccc1be1d5e1b
omer7212 <omershala77@gmail.com>
2026-03-10 17:42:56 +0100

    module27

---
+++ module27/main.js
@@ -0,0 +1,19 @@
+function displayAlert(){
+
+alert('This text is inside the function!')
+
+
+}
+
+
+displayAlert()
+
+
+function mbledhja(numri1,numri2){
+
+return(numri1 + numri2);
+
+
+}
+
+document.write((5+8));
