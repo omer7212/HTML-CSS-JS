2c551420dc8bd4591a8550b68194a1e2612ae240
omer7212 <omershala77@gmail.com>
2026-03-24 17:47:29 +0100

    module29

---
+++ module29/main.js
@@ -0,0 +1,48 @@
+var programmingLanguages = ["Javascript " ,"PHP", "Python"]
+console.log(programmingLanguages);
+
+
+
+var numrat = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(numrat);
+
+
+
+
+
+
+console.log(programmingLanguages[1]);
+
+// array function
+
+// push add an elemente at the end of the list
+
+programmingLanguages.push("Java");
+
+
+numrat.push(11);
+
+
+
+document.write(programmingLanguages)
+
+
+document.write(numrat)
+
+
+// pop delete the last element
+
+numrat.pop();
+
+console.log(numrat);
+
+
+// unshift - add an element at the begining of the list
+
+
+programmingLanguages.unshift("C#");
+
+console.log(programmingLanguages);
+
+
