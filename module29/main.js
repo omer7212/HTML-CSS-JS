1e0f922f079230d42577e5a9af07fe346c6dad42
omer7212 <omershala77@gmail.com>
2026-03-26 17:16:40 +0100

    module29

--- module29/main.js
+++ module29/main.js
@@ -46,3 +46,56 @@ programmingLanguages.unshift("C#");
 console.log(programmingLanguages);
 
 
+programmingLanguages.shift();
+console.log(programmingLanguages);
+
+
+
+
+
+
+
+programmingLanguages.splice(1,2, 'Ruby');
+console.log(programmingLanguages);
+
+
+
+var students = ["D", "O" , "R" , "N", "S"];
+
+var  [s1 , s2, s3 ,s4 , s5] = students;
+
+console.log(s1)
+console.log(s2)
+console.log(s3)
+console.log(s4)
+console.log(s5)
+
+
+
+var qytete = ['Prishtina', 'Tirana' , 'Paris', 'Madrid', 'Barcelona','Berlin','London','New York','Tokyo','Instanbul','Mumbai','Rome', 'Lisbon','Budapest','Valleta','Munich','Florence','Bergen','Amsterdam','Porto'];
+
+
+var [q1 , q2 , q3 , q4 ,q5 , q6 , q7 , q8 , q9 , q10 , q11 , q12 , q13 , q14 , q15 , q16 , q17 , q18 , q19 ,q20] = qytete;
+
+
+console.log(q1)
+console.log(q2)
+console.log(q3)
+console.log(q4)
+console.log(q5)
+console.log(q6)
+console.log(q7)
+console.log(q8)
+console.log(q9)
+console.log(q10)
+console.log(q11)
+console.log(q12)
+console.log(q13)
+console.log(q14)
+console.log(q15)
+console.log(q16)
+console.log(q17)
+console.log(q18)
+console.log(q19)
+console.log(q20)
+
