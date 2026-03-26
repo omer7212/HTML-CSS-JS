0895cb00cd625c884fddfd6848042697c425a22e
omer7212 <omershala77@gmail.com>
2026-03-26 17:43:52 +0100

    module30

---
+++ module30/main.js
@@ -0,0 +1,36 @@
+// var mosha = 7
+
+
+// if(mosha<8){
+
+// document.write("Your child is to young for programming")
+
+
+// } else{
+
+// document.write("We will inform you about the group.")
+
+// }
+
+
+
+var age = 20
+
+
+if(age<8){
+
+document.write("Your child is to young for programming")
+
+}
+
+else if(age>18){
+
+document.write("Your child is to old for programming")
+
+} else{
+
+document.write("We will inform you about the group.")
+
+}
+
+
