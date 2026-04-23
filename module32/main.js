e7aa69423a7ac046e9606f16863b555ae71096ec
omer7212 <omershala77@gmail.com>
2026-04-23 17:42:13 +0200

    module32

---
+++ module32/main.js
@@ -0,0 +1,60 @@
+var input = document.getElementById("input_id");
+var button = document.getElementById("btn_id");
+var text = document.getElementById("tekst_id");
+
+
+
+button.onclick = function(){
+text.innerHTML = input.value;
+
+}
+
+
+
+// var text = "The best school in the world is Digital School";
+// var result = text.search("Digital School");
+// document.getElementById("result1").innerHTML = result;
+
+
+
+// var text = "The best school in the world is Digital School";
+// var result = text.search(/Digital School/);
+// document.getElementById("result2").innerHTML = result;
+
+// var text = "The best school in the world is Digital School";
+// var reesult = text.replace(/Digital School/,"Another School");
+// document.getElementById("result3").innerHTML = result;
+
+
+// var text = "abcdef"
+// var regex = new RegExp('abc')
+// document.getElementById('result4').innerHTML = regex.test(text);
+
+
+// var text = "My school is the best school in the world";
+// var regex = /school/g;
+// document.getElementById('result5').innerHTML = text.match(regex);
+
+
+var text = " Digital school is the best school in the world"
+var regex = /i/g;
+document.getElementById('result6').innerHTML = text.match(regex);
+
+
+var text = " Digital school is the best school in the world"
+var regex = /[abc]/g;
+document.getElementById('result7').innerHTML = text.match(regex);
+
+
+
+var text =  " Digital school is in top 10 the best school in the world"
+var regex = /[0-9]/g;
+document.getElementById('result8').innerHTML = text.match(regex);
+
+var text = "My school is the best school in the world";
+var regex = /(top|best|school)/g;
+document.getElementById('result9').innerHTML = text.match(regex);
+
+var text = "100 percent";
+var regex = /\d/g;
+document.getElementById('result10').innerHTML = text.match(regex);
